178203c6a40685147811157d8788abd5d4ccddd0
shivam prajapati <shivamkumarprajapati777@gmail.com>
2026-07-16 11:34:33 +0530

    Time: 7 ms (9.95%), Space: 46.8 MB (52.3%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,17 +11,31 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        // slow and fast pointer base approach 
-        // if there is a cycle then slow and falst will definately meet at any point 
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast!=null && fast.next!=null) {
-            slow=slow.next;
-            fast = fast.next.next;
-            if(slow==fast) {
+        // brute force appraoch 
+        // using hashset to store teh address of the nodes 
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode current=head;
+        while(current!=null) {
+            if(set.contains(current)) {
                 return true;
             }
-        }
+            set.add(current);
+            current=current.next;
+
+        } 
+
         return false;
+        // slow and fast pointer base approach 
+        // if there is a cycle then slow and falst will definately meet at any point 
+        // ListNode slow = head;
+        // ListNode fast = head;
+        // while(fast!=null && fast.next!=null) {
+        //     slow=slow.next;
+        //     fast = fast.next.next;
+        //     if(slow==fast) {
+        //         return true;
+        //     }
+        // }
+        // return false;
     }
 }
